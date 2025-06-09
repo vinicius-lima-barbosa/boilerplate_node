89413a80b2a41a5122cac8535b562063337cb951
vinicius-lima-barbosa <viniciusbarbosa0202@gmail.com>
2025-06-09 13:14:31 -0300

    refact: convert auth middleware functions to AuthMiddleware class

--- src/shared/middlewares/authMiddleware.ts
+++ src/shared/middlewares/authMiddleware.ts
@@ -2,40 +2,75 @@ import { HttpError } from '../errors/httpError';
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 import nodemailer from 'nodemailer';
 
-export const authMiddleware = async (
-  request: FastifyRequest,
-  _reply: FastifyReply
-) => {
-  const token = request.cookies.token;
-  if (!token) throw new HttpError(401, 'Authentication token missing');
+export class AuthMiddleware {
+  constructor(private readonly app: FastifyInstance) {}
 
-  const payload = await request.jwtVerify<{ id: string }>();
-  request.user = { id: payload.id };
-};
+  public handle = async (request: FastifyRequest, _reply: FastifyReply) => {
+    const token = request.cookies.token;
+    if (!token) throw new HttpError(401, 'Authentication token missing');
 
-export const sendEmailVerification = async (
-  app: FastifyInstance,
-  email: string,
-  link: string
-) => {
-  const { SMTP_USER, SMTP_PASSWORD } = app.config;
+    const payload = await request.jwtVerify<{ id: string }>();
+    request.user = { id: payload.id };
+  };
 
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: SMTP_USER,
-      pass: SMTP_PASSWORD
-    }
-  });
+  public sendEmailVerification = async (email: string, link: string) => {
+    const { SMTP_USER, SMTP_PASSWORD } = this.app.config;
 
-  await transporter.sendMail({
-    from: SMTP_USER,
-    to: email,
-    replyTo: SMTP_USER,
-    subject: 'Confirm your email',
-    text: `Click the link to confirm your email.`,
-    // O ideal seria enviar o link para a página de Login e então dar um GET na rota de confirmação de Email.
-    // Mas por enquanto, vamos enviar o link direto para a confirmação.
-    html: `<p>Click the link to confirm your email: <a href="${link}">Link para Confirmar o Email</a></p>`
-  });
-};
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: SMTP_USER,
+        pass: SMTP_PASSWORD
+      }
+    });
+
+    await transporter.sendMail({
+      from: SMTP_USER,
+      to: email,
+      replyTo: SMTP_USER,
+      subject: 'Confirm your email',
+      text: `Click the link to confirm your email.`,
+      // O ideal seria enviar o link para a página de Login e então dar um GET na rota de confirmação de Email.
+      // Mas por enquanto, vamos enviar o link direto para a confirmação.
+      html: `<p>Click the link to confirm your email: <a href="${link}">Link para Confirmar o Email</a></p>`
+    });
+  };
+}
+
+// export const authMiddleware = async (
+//   request: FastifyRequest,
+//   _reply: FastifyReply
+// ) => {
+//   const token = request.cookies.token;
+//   if (!token) throw new HttpError(401, 'Authentication token missing');
+
+//   const payload = await request.jwtVerify<{ id: string }>();
+//   request.user = { id: payload.id };
+// };
+
+// export const sendEmailVerification = async (
+//   app: FastifyInstance,
+//   email: string,
+//   link: string
+// ) => {
+//   const { SMTP_USER, SMTP_PASSWORD } = app.config;
+
+//   const transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//       user: SMTP_USER,
+//       pass: SMTP_PASSWORD
+//     }
+//   });
+
+//   await transporter.sendMail({
+//     from: SMTP_USER,
+//     to: email,
+//     replyTo: SMTP_USER,
+//     subject: 'Confirm your email',
+//     text: `Click the link to confirm your email.`,
+//     // O ideal seria enviar o link para a página de Login e então dar um GET na rota de confirmação de Email.
+//     // Mas por enquanto, vamos enviar o link direto para a confirmação.
+//     html: `<p>Click the link to confirm your email: <a href="${link}">Link para Confirmar o Email</a></p>`
+//   });
+// };
