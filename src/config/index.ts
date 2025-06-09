563694201368e597f680f931e616466e4cf43307
vinicius-lima-barbosa <viniciusbarbosa0202@gmail.com>
2025-06-09 01:24:58 -0300

    refactor: convert configLoader function to ConfigLoader class for improved structure

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,18 @@
 import { FastifyInstance } from 'fastify';
 import { registerEnv } from './env';
 import { swaggerConfig } from './swagger';
+import { registerPrisma } from './prismaClient';
 
-export async function configLoader(app: FastifyInstance) {
-  await registerEnv(app);
-  await swaggerConfig(app);
+export class ConfigLoader {
+  public app: FastifyInstance;
+
+  constructor(app: FastifyInstance) {
+    this.app = app;
+  }
+
+  async load() {
+    await registerEnv(this.app);
+    await swaggerConfig(this.app);
+    await registerPrisma(this.app, {});
+  }
 }
