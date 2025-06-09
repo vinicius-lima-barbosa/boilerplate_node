ef15f01d39e4ebb4b7fa39fd82d95559279a8c05
vinicius-lima-barbosa <viniciusbarbosa0202@gmail.com>
2025-06-09 01:25:43 -0300

    refactor: convert userController function to UserController class for improved structure

--- src/modules/users/controllers/userController.ts
+++ src/modules/users/controllers/userController.ts
@@ -1,82 +1,51 @@
-import { FastifyInstance } from 'fastify';
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 import { UserService } from '../services/userService';
 import EditUserDto, { EditUserSchema } from '../dtos/editUserDto';
-import { authMiddleware } from '../../../shared/middlewares/authMiddleware';
-import {
-  DeleteUserSchema,
-  EditUserByIdSchema,
-  ListUserByIdSchema,
-  UsersListSchema
-} from '../schemas/userSchemas';
 
-export async function userController(app: FastifyInstance) {
-  const service = new UserService(app);
+export class UserController {
+  private service: UserService;
 
-  app.get(
-    '/users',
-    {
-      preHandler: [authMiddleware],
-      schema: UsersListSchema
-    },
-    async (_request, reply) => {
-      try {
-        const users = await service.list();
-        reply.send(users);
-      } catch (error) {
-        throw error;
-      }
+  constructor(private readonly app: FastifyInstance) {
+    this.service = new UserService(this.app);
+  }
+
+  public getUsers = async (_request: FastifyRequest, reply: FastifyReply) => {
+    try {
+      const users = await this.service.list();
+      reply.status(200).send(users);
+    } catch (error) {
+      throw error;
     }
-  );
+  };
 
-  app.get(
-    '/users/:id',
-    {
-      preHandler: [authMiddleware],
-      schema: ListUserByIdSchema
-    },
-    async (request, reply) => {
-      try {
-        const { id } = request.params as { id: string };
-        const user = await service.listById(id);
-        reply.status(200).send(user);
-      } catch (error) {
-        throw error;
-      }
+  public getUserById = async (request: FastifyRequest, reply: FastifyReply) => {
+    try {
+      const { id } = request.params as { id: string };
+      const user = await this.service.listById(id);
+      reply.status(200).send(user);
+    } catch (error) {
+      throw error;
     }
-  );
+  };
 
-  app.put(
-    '/users/:id',
-    {
-      preHandler: [authMiddleware],
-      schema: EditUserByIdSchema
-    },
-    async (request, reply) => {
-      try {
-        const { id } = request.params as { id: string };
-        const dto = app.validate<EditUserDto>(EditUserSchema, request.body);
-        const user = await service.editUser(id, dto);
-        reply.status(200).send(user);
-      } catch (error) {
-        throw error;
-      }
+  public editUser = async (request: FastifyRequest, reply: FastifyReply) => {
+    try {
+      const { id } = request.params as { id: string };
+      const dto = this.app.validate<EditUserDto>(EditUserSchema, request.body);
+      const user = await this.service.editUser(id, dto);
+      reply.status(200).send(user);
+    } catch (error) {
+      throw error;
     }
-  );
+  };
 
-  app.delete(
-    '/users/:id',
-    {
-      preHandler: [authMiddleware],
-      schema: DeleteUserSchema
-    },
-    async (request, reply) => {
-      try {
-        const { id } = request.params as { id: string };
-        await service.deleteUser(id);
-        reply.status(200).send({ message: 'User deleted' });
-      } catch (error) {
-        throw error;
-      }
+  public deleteUser = async (request: FastifyRequest, reply: FastifyReply) => {
+    try {
+      const { id } = request.params as { id: string };
+      await this.service.deleteUser(id);
+      reply.status(200).send({ message: 'User deleted' });
+    } catch (error) {
+      throw error;
     }
-  );
+  };
 }
