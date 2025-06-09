4631b55d0612dfc7f815bf2c68626359ddc01ace
vinicius-lima-barbosa <viniciusbarbosa0202@gmail.com>
2025-06-09 01:25:18 -0300

    refactor: convert authRoutes function to AuthRoutes class for improved structure

--- src/modules/auth/routes.ts
+++ src/modules/auth/routes.ts
@@ -1,6 +1,17 @@
 import { FastifyInstance } from 'fastify';
-import { authController } from './controllers/authController';
+import { AuthController } from './controllers/authController';
 
-export async function authRoutes(app: FastifyInstance) {
-  await authController(app);
+export class AuthRoutes {
+  private controller: AuthController;
+
+  constructor(private app: FastifyInstance) {
+    this.controller = new AuthController(this.app);
+  }
+
+  public register() {
+    this.app.post('/auth/register', this.controller.register);
+    this.app.get('/auth/verify-email', this.controller.verifyEmail);
+    this.app.post('/auth/login', this.controller.login);
+    this.app.get('/auth/logout', this.controller.logout);
+  }
 }
