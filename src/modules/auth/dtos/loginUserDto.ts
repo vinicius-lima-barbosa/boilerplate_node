c797fd552af8506c9c499114fb7fefbb0391697f
vinicius-lima-barbosa <viniciusbarbosa0202@gmail.com>
2025-06-09 15:30:54 -0300

    fix: delete unused files

--- src/modules/auth/dtos/loginUserDto.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-export const LoginUserSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(6)
-});
-
-type LoginUserDto = z.infer<typeof LoginUserSchema>;
-export default LoginUserDto;
